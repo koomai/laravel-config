4ce7c51cb250dd86e62b2144d7c56dad7c6043b7
Sid K <sid@koomai.net>
2019-05-25 00:47:59 +1000

    removed conditional for different versions - now moved to a different branch

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -15,10 +15,6 @@ class TestCase extends OrchestraTestCase
 
         $this->withFactories(__DIR__ . '/factories');
 
-        // Call run() method on new versions of Laravel
-        $migrate = $this->artisan('migrate', ['--database' => 'testing']);
-        if (!is_int($migrate)) {
-            $migrate->run();
-        }
+        $this->artisan('migrate', ['--database' => 'testing']);
     }
 }
